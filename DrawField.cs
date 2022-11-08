1ea85b694cf742da540b9d537abbbb2547ea8a8b
Makarov Oleg <bogdanolegom@gmail.com>
2022-11-08 18:20:48 +0300

    Add method

--- DrawField.cs
+++ DrawField.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,39 @@ namespace CourseWork_LeeAlgorithm
 {
     internal class DrawField
     {
-        public static void setMapRanges(Field traceField, System.Windows.Forms.Button[,] traceTiles)
+        public static void SetMapRanges(Field traceField, System.Windows.Forms.Button[,] traceTiles)
         {
-
+            for (int i = 0; i < traceField.N; i++)
+            {
+                for (int j = 0; j < traceField.M; j++)
+                {
+                    if (traceField.ArrayField[i, j] > 0)
+                    {
+                        traceTiles[i, j].Text = traceField.ArrayField[i, j].ToString();
+                    }
+                }
+            }
         }
+
+        public static void ClearTiles(System.Windows.Forms.Button[,] tiles, Field field)
+        {
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    tiles[i, j].Text = " ";
+                    if (field.ArrayField[i, j] > 1)
+                    {
+                        field.ArrayField[i, j] = 0;
+                        if (!(i == field.FinishN && j == field.FinishM))
+                        {
+                            tiles[i, j].BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
